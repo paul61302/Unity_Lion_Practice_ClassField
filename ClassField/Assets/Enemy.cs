28477cf2ccf1d1499acda2aa5fa62fdcfd3f2f69
Paul Paul <paul61302@gmail.com>
2020-04-08 22:20:10 +0800

    2020/4/8 C# hw new

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,72 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// 血量
+    /// </summary>
+    [Header("血量")]
+    [Tooltip("怪物的血量")]
+    [Range(50, 500)]
+    public int HP;
+    /// <summary>
+    /// 魔力
+    /// </summary>
+    [Header("魔力")]
+    [Tooltip("怪物的魔力")]
+    [Range(10, 100)]
+    public int MP = 100;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [Header("能力值")]
+    //[Tooltip("怪物的血量")]
+    [Range(1, 50.5f)]
+    public float MoveSpeed = 10.5f;
+    /// <summary>
+    /// 攻擊力
+    /// </summary>
+    //[Header("血量")]
+    //[Tooltip("怪物的血量")]
+    [Range(20, 200)]
+    public int Att = 50;
+    /// <summary>
+    /// 防禦力
+    /// </summary>
+    //[Header("血量")]
+    //[Tooltip("怪物的血量")]
+    [Range(0, 100)]
+    public int Def;
+    /// <summary>
+    /// 武器名稱
+    /// </summary>
+    [Header("裝備")]
+    //[Tooltip("怪物的血量")]
+    //[Range(50, 500)]
+    public string Weapon;
+    /// <summary>
+    /// 裝備名稱
+    /// </summary>
+    //[Header("血量")]
+    //[Tooltip("怪物的血量")]
+    //[Range(50, 500)]
+    public string Equipment = ("皮衣");
+    /// <summary>
+    /// 是否帶有鑰匙
+    /// </summary>
+    [Header("其他資料")]
+    [Tooltip("怪物是否帶有鑰匙")]
+    //[Range(50, 500)]
+    public bool Key = false;
+    /// <summary>
+    /// 血量
+    /// </summary>
+    //[Header("血量")]
+    [Tooltip("怪物是否帶有寶物")]
+    //[Range(50, 500)]
+    public bool Treasure = false;
+
+
+
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物一號")]
+    public Enemy Enemy1;
+
+    [Header("怪物二號")]
+    public Enemy Enemy2;
+
+    private void Start()
+    {
+        //取得怪物資訊
+        print("怪物一號的魔力為：" + Enemy1.MP);
+        print("怪物二號的裝備名稱為：" + Enemy2.Equipment);
+
+        //修改怪物資訊
+        Enemy1.HP = 100;
+        Enemy2.HP = 300;
+        Enemy1.Def = 50;
+        Enemy1.Weapon = "木棍";
+        Enemy2.Weapon = "短刀";
+        Enemy1.Key = true;
+        Enemy2.Treasure = true;
+
+
+    }
+
+}
